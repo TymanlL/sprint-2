539f591bc0468ba8f1aac4a050db0c5881c426a2
Евген <foryouonlyused@gmail.com>
2024-12-12 22:42:43 +0300

    Delete sharding-repl-cache directory

--- sharding-repl-cache/scripts/print.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-docker compose exec -T shard1_db1 mongosh --port 27018 --quiet <<EOF
-\`shard count=\${rs.status().members.length}\`
-EOF
-
-docker compose exec -T shard2_db1 mongosh --port 27019 --quiet <<EOF
-\`shard count=\${rs.status().members.length}\`
-EOF
